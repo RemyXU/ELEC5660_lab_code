50f9da85322c293c35dddd186fd97f60d4af87f9
Remy XU <xuhr001@sina.com>
2017-11-08 01:40:21 +0800

    Update trajectory.h

--- src/trajectory_node/src/trajectory.h
+++ src/trajectory_node/src/trajectory.h
@@ -4,59 +4,9 @@
 #include <Eigen/Eigen>
 #include <stdio.h>
 
-/*
- * this function is to get desired states for specific trajectory, just generated, at time dt.
- * input:
- * dT   -> the time
- * hover_pos -> the desired position where you want quadrotor to hover
- * now_vel -> maybe useless
- *
- * output:
- * desired_pos -> desired position at dT
- * desired_vel -> desired velocity at dT
- * desired_acc -> desired acceleration at dT
- * return:
- * true  -> you have alread configured desired states
- * false -> no desired state
- */
-bool
-trajectory_control( const double dT,
-                    const Eigen::Vector3d hover_pos,
-                    const Eigen::Vector3d now_vel,
-                    double& end_time,
-                    Eigen::Vector3d& desired_pos,
-                    Eigen::Vector3d& desired_vel,
-                    Eigen::Vector3d& desired_acc )
-{
-    // if you don't want to use Eigen, then you can use these arrays
-    // or you can delete them and use Eigen
-    double hover_p[3], now_v[3];
-    double desired_p[3], desired_v[3], desired_a[3];
-    hover_p[0] = hover_pos.x( );
-    hover_p[1] = hover_pos.y( );
-    hover_p[2] = hover_pos.z( );
-    now_v[0]   = now_vel.x( );
-    now_v[1]   = now_vel.y( );
-    now_v[2]   = now_vel.z( );
-    // your code // please use coefficients from matlab to get desired states
-  
     const int R = 8; // Using R - 1 th order polynomial for the trajectory
     const int v_limit = 0.5;  // maximum velocity
     const int W = 9; // number of waypoints
-
-    printf( "x = %.2f, y = %.2f, z = %.2f\n", desired_p[0], desired_p[1], desired_p[2] );
-
-    /*---------------------------------------------------------------------------------*/
-    /*--- YOUR CODE FROM HERE --- YOUR CODE FROM HERE --- YOUR CODE FROM HERE ---------*/
-    /*---------------------------------------------------------------------------------*/
-    //	printf("dt= %.2f\n",dT);
-    double max_run_time = 25.0; // set your trajectory max run time.
-    double end_position[3];     // set your trajectory end point.
-
-    end_position[0] = 0.0;
-    end_position[1] = 2.0;
-    end_position[2] = 0.5;
-    
     const double path1[3*W] = { 0.0, 0.0, 0.5, \
                                -0.2, 0.2, 0.7, \
                                -0.5, 0.5, 0.9, \
@@ -95,7 +45,59 @@ trajectory_control( const double dT,
                                   1.0000,-0.0242,-0.0115, 0.0014, 0.0010, 0.0000, 0.0000, 0.0000, \
                                   0.9000,-0.0189, 0.0020,-0.0048,-0.0013, 0.0002, 0.0000, 0.0000, \
                                   0.7000,-0.1274,-0.0063, 0.0117, 0.0016,-0.0007,-0.0002, 0.0000, };
+
+
+/*
+ * this function is to get desired states for specific trajectory, just generated, at time dt.
+ * input:
+ * dT   -> the time
+ * hover_pos -> the desired position where you want quadrotor to hover
+ * now_vel -> maybe useless
+ *
+ * output:
+ * desired_pos -> desired position at dT
+ * desired_vel -> desired velocity at dT
+ * desired_acc -> desired acceleration at dT
+ * return:
+ * true  -> you have alread configured desired states
+ * false -> no desired state
+ */
+bool
+trajectory_control( const double dT,
+                    const Eigen::Vector3d hover_pos,
+                    const Eigen::Vector3d now_vel,
+                    double& end_time,
+                    Eigen::Vector3d& desired_pos,
+                    Eigen::Vector3d& desired_vel,
+                    Eigen::Vector3d& desired_acc )
+{
+    // if you don't want to use Eigen, then you can use these arrays
+    // or you can delete them and use Eigen
+    double hover_p[3], now_v[3];
+    double desired_p[3], desired_v[3], desired_a[3];
+    hover_p[0] = hover_pos.x( );
+    hover_p[1] = hover_pos.y( );
+    hover_p[2] = hover_pos.z( );
+    now_v[0]   = now_vel.x( );
+    now_v[1]   = now_vel.y( );
+    now_v[2]   = now_vel.z( );
+    // your code // please use coefficients from matlab to get desired states
   
+
+
+    printf( "x = %.2f, y = %.2f, z = %.2f\n", desired_p[0], desired_p[1], desired_p[2] );
+
+    /*---------------------------------------------------------------------------------*/
+    /*--- YOUR CODE FROM HERE --- YOUR CODE FROM HERE --- YOUR CODE FROM HERE ---------*/
+    /*---------------------------------------------------------------------------------*/
+    //	printf("dt= %.2f\n",dT);
+    double max_run_time = 25.0; // set your trajectory max run time.
+    double end_position[3];     // set your trajectory end point.
+
+    end_position[0] = 2.0;
+    end_position[1] = 0.0;
+    end_position[2] = 0.5;
+   
     int w=0;  //The segment where dT is 0~7
     for (int i = 0; i < W; i++) 
     {
@@ -142,16 +144,6 @@ trajectory_control( const double dT,
         desired_acc.y( ) = desired_a[1];
         desired_acc.z( ) = desired_a[2];
         end_time         = max_run_time;
-      
-      
-      //std::cout << "Time: " << dT << ".\n";
-      //std::cout << "x: " << desired_p[0] << "\n";
-      //std::cout << "y: " << desired_p[1] << "\n";
-      //std::cout << "z: " << desired_p[2] << "\n";
-      //std::cout << "pos_x: " << hover_p[0] << "\n";
-      //std::cout << "pos_y: " << hover_p[1] << "\n";
-      //std::cout << "pos_z: " << hover_p[2] << "\n";
-      
       
         return true; // if you have got desired states, true.
     }
